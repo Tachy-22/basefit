49fc5f9b53096efb2add3dcfb3bf033e4fd72f1c
Tachy-22 <entekumejeffrey@gmail.com>
2024-10-20 13:25:52 +0100

    push

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -1,25 +1,61 @@
 "use client";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 import { base } from "viem/chains";
 import { WagmiProvider } from "wagmi";
 import { NEXT_PUBLIC_CDP_API_KEY } from "../config";
-import { useWagmiConfig } from "../wagmi";
+import "@rainbow-me/rainbowkit/styles.css";
+
+//import { useWagmiConfig } from "../wagmi";
+import {
+  metaMaskWallet,
+  rainbowWallet,
+  coinbaseWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
 
 type Props = { children: ReactNode };
 
 const queryClient = new QueryClient();
 
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended Wallet",
+      wallets: [coinbaseWallet],
+    },
+    {
+      groupName: "Other Wallets",
+      wallets: [rainbowWallet, metaMaskWallet],
+    },
+  ],
+  {
+    appName: "onchainkit",
+    projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID as string,
+  }
+);
+
+const wagmiConfig = getDefaultConfig({
+  appName: "onchainkit",
+  connectors,
+  projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID as string,
+  chains: [base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
 function OnchainProviders({ children }: Props) {
-  const wagmiConfig = useWagmiConfig();
+  // const wagmiConfig = useWagmiConfig();
 
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
-          <div  className=" max-w-screen w-screen h-full ">
+          <div className=" max-w-screen w-screen h-full ">
             {" "}
             <RainbowKitProvider modalSize="wide">{children}</RainbowKitProvider>
           </div>
