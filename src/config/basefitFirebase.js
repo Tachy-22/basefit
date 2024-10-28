5caca5f2e9e4298cedd96bdc2210a0b2c6d3eb59
Tachy-22 <entekumejeffrey@gmail.com>
2024-10-28 15:08:18 +0100

    hid environment variables and firebase keys

--- src/config/basefitFirebase.js
+++ src/config/basefitFirebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyApfg07eE7zNik68hyceuahWUMHxvnqP4U",
-    authDomain: "basefit-app.firebaseapp.com",
-    projectId: "basefit-app",
-    storageBucket: "basefit-app.appspot.com",
-    messagingSenderId: "961856782213",
-    appId: "1:961856782213:web:37b17befaff9bdf7ee4eec",
-    measurementId: "G-QSG7YNJYE0"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
