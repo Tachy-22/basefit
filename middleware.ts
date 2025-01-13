dd73f4b8781e447e29618620a9259ffa9ded892e
Entekume Jeffery <100496531+Tachy-22@users.noreply.github.com>
2025-01-13 12:15:54 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  //  console.log("third log", request.nextUrl.pathname); // => { name: 'vercel', value: 'fast', Path: '/' }
+ 
 
   // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
   // Getting cookies from the request using the `RequestCookies` API
+
   let isWalletConnectedCookie = request.cookies.get("wallet-connection");
-  //  console.log("first log", isWalletConnectedCookie); // => { name: 'nextjs', value: 'fast', Path: '/' }
+
 
   if (
     request.nextUrl.pathname.startsWith("/user") &&
